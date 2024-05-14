d7019151c0d52b34bd48688063d321cd03f8ac4c
Owen <96526481+owencz1998@users.noreply.github.com>
2024-05-14 18:41:16 +0100

    Update MainActivity.java

--- android/app/src/main/java/com/atomm/MainActivity.java
+++ android/app/src/main/java/com/atomm/MainActivity.java
@@ -1,4 +1,4 @@
-package com.atomm.open;
+package com.YouTune.open;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,7 +10,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Atomm";
+    return "YouTune";
   }
 
   // @Override
